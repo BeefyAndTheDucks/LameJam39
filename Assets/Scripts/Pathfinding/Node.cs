2942ed70f00f39cb5f11c84774d55381e6578ddd
BeefyAndTheDucks <simonsej123gamerminecraft@gmail.com>
2024-05-17 17:10:55 +0200

    Start Pathfinding system

---
+++ Assets/Scripts/Pathfinding/Node.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Node
+{
+    public bool walkable;
+    public Vector2 worldPosition;
+
+    public Node(bool _walkable, Vector2 _worldPosition)
+    {
+        walkable = _walkable;
+        worldPosition = _worldPosition;
+    }
+}
