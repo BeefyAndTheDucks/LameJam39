619fb1f9ae2566b7e758f815207564e595ca23bb
BeefyAndTheDucks <simonsej123gamerminecraft@gmail.com>
2024-05-18 14:40:24 +0200

    Finish context menu + Regenerate NavMesh on changed tile

--- Assets/Scripts/ContextMenuHandler.cs
+++ Assets/Scripts/ContextMenuHandler.cs
@@ -1,6 +1,8 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 public class ContextMenuHandler : MonoBehaviour
 {
@@ -10,10 +12,23 @@ public class ContextMenuHandler : MonoBehaviour
     [SerializeField] private Tilemap enemyTilemap;
     [SerializeField] private Tilemap resourceTilemap;
     [SerializeField] private Grid grid;
+    [SerializeField] private RectTransform contextMenuTransform;
+    [SerializeField] private Button contextActionPrefab;
+    [SerializeField] private TMP_Text titleText;
+
+    public bool IsContextMenuEnabled => contextMenuTransform.gameObject.activeSelf;
 
     private void Start()
     {
         GameManager.Instance.OnShowContextMenu += OnShowContextMenu;
+        GameManager.Instance.OnChooseMove += OnChooseMove;
+
+        HideContextMenu();
+    }
+
+    private void OnChooseMove(Vector2 vector)
+    {
+        HideContextMenu();
     }
 
     private void OnShowContextMenu(Vector2 clickedPosition)
@@ -49,6 +64,7 @@ public class ContextMenuHandler : MonoBehaviour
         {
             // There is a resource tile. Create a mine context menu.
             CreateMineContextMenu(cellCoordinate, resourceTilemap);
+            return;
         }
 
         // There will always be ground. But a defensive check can't hurt
@@ -61,28 +77,80 @@ public class ContextMenuHandler : MonoBehaviour
         CreateBuildContextMenu(cellCoordinate);
     }
 
+    private void FinishContextMenu(Vector3Int cellCoordinate)
+    {
+        CreateCancelButton();
+
+        //Vector3 position = grid.CellToWorld(cellCoordinate);
+        //Vector3 screenPosition = RectTransformUtility.WorldToScreenPoint(GameManager.Instance.cachedCameraMain, position);
+        Vector3 screenPosition = Input.mousePosition;
+        contextMenuTransform.position = screenPosition;
+        contextMenuTransform.position += (Vector3)contextMenuTransform.sizeDelta / 1.5f;
+
+        contextMenuTransform.gameObject.SetActive(true);
+    }
+
+    private void BeginContextMenu(string title)
+    {
+        foreach (Transform child in contextMenuTransform)
+        {
+            if (child != titleText.transform)
+                Destroy(child.gameObject);
+        }
+
+        titleText.text = title;
+    }
+
+    private void HideContextMenu()
+    {
+        contextMenuTransform.gameObject.SetActive(false);
+    }
+
+    private void ButtonActionWrapper(Action action)
+    {
+        action?.Invoke();
+        HideContextMenu();
+    }
+
+    private void CreateCancelButton() => CreateButton(HideContextMenu, "Cancel", Color.red);
+
+    private Button CreateButton(Action buttonAction, string buttonText, Color buttonColor)
+    {
+        Button createdButton = Instantiate(contextActionPrefab, contextMenuTransform);
+        createdButton.image.color = buttonColor;
+        createdButton.GetComponentInChildren<TMP_Text>().text = buttonText;
+        createdButton.onClick.AddListener(buttonAction.Invoke);
+        return createdButton;
+    }
+
     private void CreateBuildContextMenu(Vector3Int cellCoordinate)
     {
-        throw new NotImplementedException();
+        BeginContextMenu("Build");
+        FinishContextMenu(cellCoordinate);
     }
 
-    private void CreateMineContextMenu(Vector3Int cellCoordinate, Tilemap resourceTilemap)
+    private void CreateMineContextMenu(Vector3Int cellCoordinate, Tilemap tilemap)
     {
-        throw new NotImplementedException();
+        BeginContextMenu("Resource");
+        FinishContextMenu(cellCoordinate);
     }
 
-    private void CreateBuildingConfigurationContextMenu(Vector3Int cellCoordinate, Tilemap buildingTilemap)
+    private void CreateBuildingConfigurationContextMenu(Vector3Int cellCoordinate, Tilemap tilemap)
     {
-        throw new NotImplementedException();
+        BeginContextMenu("Configure");
+        FinishContextMenu(cellCoordinate);
     }
 
-    private void CreateAttackContextMenu(Vector3Int cellCoordinate, Tilemap enemyTilemap)
+    private void CreateAttackContextMenu(Vector3Int cellCoordinate, Tilemap tilemap)
     {
-        throw new NotImplementedException();
+        BeginContextMenu("Enemy");
+        FinishContextMenu(cellCoordinate);
     }
 
-    private void CreateDestructionContextMenu(Vector3Int cellCoordinate, Tilemap unwalkableTilemap)
+    private void CreateDestructionContextMenu(Vector3Int cellCoordinate, Tilemap tilemap)
     {
-        throw new NotImplementedException();
+        BeginContextMenu("Obstacle");
+        CreateButton(() => ButtonActionWrapper(() => tilemap.SetTile(cellCoordinate, null)), "Deconstruct", Color.green);
+        FinishContextMenu(cellCoordinate);
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,16 @@
 ﻿using System;
-using System.Collections;
+using NavMeshPlus.Components;
+using UnityEngine.AI;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.UI;
+using UnityEngine.InputSystem;
 
+[DefaultExecutionOrder(-102)]
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
@@ -11,21 +20,69 @@ public class GameManager : MonoBehaviour
     public event Action<Vector2> OnChooseMove;
     public event Action<Vector2> OnShowContextMenu;
 
-    private Camera _cachedCameraMain;
+    [HideInInspector] public Camera cachedCameraMain;
+
+    [SerializeField] private ContextMenuHandler contextMenuHandler;
+    [SerializeField] private NavMeshSurface navMesh;
+    [SerializeField] private bool useBuildNavMeshAsync;
+
+    private bool recalcNavMesh = true;
 
     private void Awake()
     {
         Instance = this;
-        _cachedCameraMain = Camera.main;
+        cachedCameraMain = Camera.main;
+
+        Tilemap.tilemapTileChanged += OnTilemapTileChanged;
     }
+
+    private void OnTilemapTileChanged(Tilemap tilemap, Tilemap.SyncTile[] arg2) => recalcNavMesh = true;
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !IsMouseOverUI)
         {
-            Vector2 clickedPosition = _cachedCameraMain.ScreenToWorldPoint(Input.mousePosition);
-            Action<Vector2> evnt = (Vector2.Distance(player.position, clickedPosition) > 2.0f) ? OnChooseMove : OnShowContextMenu;
-            evnt?.Invoke(clickedPosition);
+            Vector2 clickedPosition = cachedCameraMain.ScreenToWorldPoint(Input.mousePosition);
+            float distance = Vector2.Distance(player.position, clickedPosition);
+            if (distance > 1.5f)
+                OnChooseMove?.Invoke(clickedPosition);
+            else if (!contextMenuHandler.IsContextMenuEnabled)
+                OnShowContextMenu?.Invoke(clickedPosition);
         }
+
+        if (recalcNavMesh)
+        {
+            UpdateNavMesh();
+            //navMesh.BuildNavMesh();
+            recalcNavMesh = false;
+        }
+    }
+
+    public static bool IsMouseOverUI
+    {
+        get
+        {
+            // [Only works well while there is not PhysicsRaycaster on the Camera)
+            EventSystem eventSystem = EventSystem.current;
+            return eventSystem != null && eventSystem.IsPointerOverGameObject();
+
+            // [Works with PhysicsRaycaster on the Camera. Requires New Input System. Assumes mouse.)
+            // if (EventSystem.current == null)
+            // {
+            //     return false;
+            // }
+            // RaycastResult lastRaycastResult = ((InputSystemUIInputModule)EventSystem.current.currentInputModule).GetLastRaycastResult(Mouse.current.deviceId);
+            // const int uiLayer = 5;
+            // return lastRaycastResult.gameObject != null && lastRaycastResult.gameObject.layer == uiLayer;
+        }
+    }
+
+    private async void UpdateNavMesh()
+    {
+        Debug.Log("Updating NavMesh");
+        if (useBuildNavMeshAsync)
+            await navMesh.BuildNavMeshAsync();
+        else
+            await navMesh.UpdateNavMesh(navMesh.navMeshData);
     }
 }
